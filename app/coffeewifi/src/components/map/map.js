6f74ffb5d457027ecb7161a048fffa9612e064fb
Jeffrey Chuc <jeffreychuc@gmail.com>
2017-12-10 15:35:06 -0800

    added OS_ACTIVITY_MODE, Value:disable in xcode schema env vars to disable _nw_connection_handler block invoke error

--- app/coffeewifi/src/components/map/map.js
+++ app/coffeewifi/src/components/map/map.js
@@ -4,12 +4,36 @@ import { StyleSheet, Text, View, Button, Platform, Alert } from 'react-native';
 export default class Map extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      initialPosition: null,
+      lastPosition: null
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        let initialPosition = JSON.stringify(position);
+        this.setState({initialPosition});
+      },
+      (error) => alert(error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      let lastPosition = JSON.stringify(position);
+      this.setState({lastPosition});
+    });
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text> this is a test</Text>
+        <Text> {this.state.initialPosition}</Text>
+        <Text> {this.state.lastPosition}</Text>
       </View>
     );
   }
