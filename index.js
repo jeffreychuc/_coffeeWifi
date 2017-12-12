2a6179b5b4ab9213528768c1567fa2062b035fcc
Jeffrey Chuc <jeffreychuc@gmail.com>
2017-12-12 08:29:07 -0800

    re-integrate sentery.io

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { AppRegistry } from 'react-native';
 import configureStore from './src/store/configureStore';
 import {Provider} from 'react-redux';
 import AppViewContainer from './src/components/app/appViewContainer';
+import { Sentry } from 'react-native-sentry';
+Sentry.config('https://9722243442dd4915b14127e2bc13b8fb:ac6ec214ab21476ab03b0b147f4964b8@sentry.io/257625').install();
 
 const store = configureStore();
 
