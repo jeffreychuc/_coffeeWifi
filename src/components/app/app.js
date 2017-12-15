95132c7c24755caf421070ab9cdffd3895cb21f9
Jeffrey Chuc <jeffreychuc@gmail.com>
2017-12-14 20:02:07 -0800

    remove comment

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -39,7 +39,6 @@ export default class App extends React.Component {
 
   handleLogin() {
     this.props.login().then(action => {
-      // this.setState({currentUser :  action.currentUser});
       SInfo.setItem('currentUser', action.currentUser.accessToken, {
         sharedPreferencesName: 'accessToken',
         keychainService: 'com.rootuser.coffeewifi'
