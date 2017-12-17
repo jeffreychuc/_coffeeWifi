6f12e1e92b0db3b9ee7810d9fd1631ab38d22318
Jeffrey Chuc <jeffreychuc@gmail.com>
2017-12-17 14:43:53 -0800

    link lottie

--- android/app/src/main/java/com/coffeewifi/MainApplication.java
+++ android/app/src/main/java/com/coffeewifi/MainApplication.java
@@ -3,6 +3,7 @@ package com.coffeewifi;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.lottie.LottiePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.sentry.RNSentryPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LottiePackage(),
             new VectorIconsPackage(),
             new RNSentryPackage(MainApplication.this),
             new MapsPackage(),
